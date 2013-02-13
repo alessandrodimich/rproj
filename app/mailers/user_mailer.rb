768e43b2bf065853aa0ede90af3d9051b82cfb5f
alessandrodimich <alessandrodimich@hotmail.com>
2013-02-13 20:48:38 +0100

    defined gmail smtp settings and defined devise views

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "dimichrails@railscasts.com"
-
-  def registration_confirmation(user)
-    #@user = user > to pass a user object in the mail
-    #attachments["rails.png"] = File.read("#{Rails.root}/public/images/rails.png")
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registered")
-  end
-end
