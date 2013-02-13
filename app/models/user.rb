64e977836883aa5bf90a670971f924d4da068427
alessandrodimich <alessandrodimich@hotmail.com>
2013-02-13 01:10:32 +0100

    almost finished devise authentication
    missing email confirmation and some templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email, :case_sensitive => false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rproj::Application.routes.draw do
 
-  devise_for :users
+  authenticated :user do
+    root :to => 'home#index'
+  end
 
-  root to: "home#welcome"
+  root :to => "home#welcome"
+  devise_for :users
 
   get "home/welcome"
 
